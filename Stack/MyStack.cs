3c98c80782d9b47e9f60ccf1c5314afa16d57505
Mikkel Bauditz Hansen <mih016@edu.zealand.dk>
2025-09-10 10:32:48 +0200

    Add project files.

---
+++ Stack/MyStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class MyStack<T>
+    {
+        private List<T> _items;
+        public int Count { get { return _items.Count; } } //returnere hvor mange elementer der er i stacken
+        
+        public MyStack()
+        {
+            _items = new List<T>();
+        }
+        public void Push(T item)
+        {
+            _items.Add(item);
+        }
+        public T Pop()
+        {
+            if (_items.Count == 0)
+                throw new InvalidOperationException("Stakken er tom.");
+            int lastIndex = _items.Count - 1; //finder det sidste element i listen
+            T topitem = _items[lastIndex]; //gemmer det fundne element i topitem
+            _items.RemoveAt(lastIndex); //fjerner elementet i den fundne position
+            return topitem; //returnerer det element der blev gemt i topitem 
+        }
+
+        public T Peek()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("Stakken er tom.");
+            }
+            return _items[_items.Count - 1]; //returnerer det element der ligger i toppen af stakken
+        }
+        
+
+
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> items = new MyStack<int>();
+            items.Push(1);
+            items.Push(2);
+            items.Push(3);
+            items.Push(4);
+
+            Console.WriteLine($"Top: {items.Peek()}");
+            Console.WriteLine($"Antal: {items.Count}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Pop: {items.Pop()}");
+            Console.WriteLine($"Antal: {items.Count}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Top: {items.Peek()}");
+            Console.WriteLine($"Antal: {items.Count}");
+            Console.WriteLine();
+
+        }
+    }
+}
